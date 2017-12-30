3391d293ed01d18f00fd6f4dbed82988a3ed1856
Carling Knight <carlingjk@gmail.com>
2017-12-30 21:56:25 +0000

    Adds favicon

--- ckf_site/static.py
+++ ckf_site/static.py
@@ -6,3 +6,8 @@ static_page = Blueprint('statics', __name__, template_folder='templates')
 @static_page.route('/js/<path:path>')
 def basic_js(path):
     return send_from_directory('js', path)
+
+
+@static_page.route('/favicon')
+def favicon():
+    return send_from_directory('static', 'favicon.ico')
